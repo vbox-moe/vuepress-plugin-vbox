b3b03d310fe837324070f38ba8721ab5ca5ed63f
Il Harper <afanyiyu@hotmail.com>
2020-07-03 17:52:55 +0800

    feat: Add extendPageData Method

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-module.exports = (options, ctx) => {
-  return {
-    name: 'vuepress-plugin-vbox',
-    extendPageData($page) {
-      const {
-        _filePath, // 源文件的绝对路径
-        _computed, // 在构建期访问全局的计算属性，如：_computed.$localePath.
-        _content, // 源文件的原始内容字符串
-        _strippedContent, // 源文件剔除掉 frontmatter 的内容字符串
-        key, // 页面唯一的 hash key
-        frontmatter, // 页面的 frontmatter 对象
-        regularPath, // 当前页面遵循文件层次结构的默认链接
-        path, // 当前页面的实际链接（在 permalink 不存在时，使用 regularPath ）
-      } = $page
-      const { pages } = ctx
-    },
-  }
+function calcDepth(str) {
+  let d = 0
+  for (let i = 0; i < str.length; i++) if (str[i] === '/') d++
+  return d
+}
+
+module.exports = {
+  name: 'vuepress-plugin-vbox',
+  extendPageData($page) {
+    const { regularPath } = $page
+    if (regularPath === '/') return
+    $page.depth = calcDepth(regularPath)
+    $page.productRegularName = regularPath.split('/')[1]
+  },
 }
