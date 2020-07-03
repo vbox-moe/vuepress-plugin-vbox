2238b379256f7e5f597a546fd71923af1436d325
Il Harper <afanyiyu@hotmail.com>
2020-07-03 23:03:33 +0800

    feat: Core Update

--- src/core.ts
+++ src/core.ts
@@ -1,6 +1,13 @@
 import { OriginalPage } from './types'
 
-function calcSiteData(pages: OriginalPage[]) {}
+function calcSiteData(pages: OriginalPage[]) {
+  const regularProduct: { [key: string]: OriginalPage[] } = {}
+  for (const page of pages) {
+    if (!regularProduct[page.title]) regularProduct[page.title] = []
+    regularProduct[page.title].push(page)
+  }
+  console.info(regularProduct)
+}
 
 function calcPageData(siteData, page) {
   const sidebarItems = {}
