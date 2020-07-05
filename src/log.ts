d4cdb513ce26f40b535d512b6b2e086d11528c02
Il Harper <afanyiyu@hotmail.com>
2020-07-05 20:09:09 +0800

    feat: Log Module Update

--- src/log.ts
+++ src/log.ts
@@ -1,15 +1,23 @@
 import chalk = require('chalk')
 
-function info(msg: string) {
-  console.log(`${chalk.blue('info')} ${msg}`)
+function info(msg: string, data?: string) {
+  console.log(`${chalk.blue('info')} ${msg} ${data ? chalk.magenta(data) : ''}`)
 }
 
-function err(msg: string) {
-  console.log(`${chalk.red('err')} ${msg}`)
+function warn(msg: string, data?: string) {
+  console.log(
+    `${chalk.yellow('err')} ${msg} ${data ? chalk.magenta(data) : ''}`
+  )
 }
 
-function success(msg: string) {
-  console.log(`${chalk.green('success')} ${msg}`)
+function err(msg: string, data?: string) {
+  console.log(`${chalk.red('err')} ${msg} ${data ? chalk.magenta(data) : ''}`)
 }
 
-export { info, err, success }
+function success(msg: string, data?: string) {
+  console.log(
+    `${chalk.green('success')} ${msg} ${data ? chalk.magenta(data) : ''}`
+  )
+}
+
+export { info, warn, err, success }
