4f68f1c1369eea43edf5b4294df3f64c03fe6ba6
Il Harper <afanyiyu@hotmail.com>
2020-07-05 20:11:18 +0800

    feat: Log Module Update

--- src/log.ts
+++ src/log.ts
@@ -1,22 +1,28 @@
 import chalk = require('chalk')
 
-function info(msg: string, data?: string) {
-  console.log(`${chalk.blue('info')} ${msg} ${data ? chalk.magenta(data) : ''}`)
-}
-
-function warn(msg: string, data?: string) {
+function info(msg: string, data?: string, suffix?: string) {
   console.log(
-    `${chalk.yellow('err')} ${msg} ${data ? chalk.magenta(data) : ''}`
+    `${chalk.blue('info')} ${msg} ${data ? chalk.magenta(data) : ''} ${suffix}`
   )
 }
 
-function err(msg: string, data?: string) {
-  console.log(`${chalk.red('err')} ${msg} ${data ? chalk.magenta(data) : ''}`)
+function warn(msg: string, data?: string, suffix?: string) {
+  console.log(
+    `${chalk.yellow('err')} ${msg} ${data ? chalk.magenta(data) : ''} ${suffix}`
+  )
 }
 
-function success(msg: string, data?: string) {
+function err(msg: string, data?: string, suffix?: string) {
   console.log(
-    `${chalk.green('success')} ${msg} ${data ? chalk.magenta(data) : ''}`
+    `${chalk.red('err')} ${msg} ${data ? chalk.magenta(data) : ''} ${suffix}`
+  )
+}
+
+function success(msg: string, data?: string, suffix?: string) {
+  console.log(
+    `${chalk.green('success')} ${msg} ${
+      data ? chalk.magenta(data) : ''
+    } ${suffix}`
   )
 }
 
